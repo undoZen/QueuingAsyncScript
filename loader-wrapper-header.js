704ab1558938fd32dea016262e881492ffe3c781
Anson Yu Wang <opensource@undozen.com>
2016-06-20 01:20:53 +0800

    add loader-wrapper footer/header

--- loader-wrapper-header.js
+++ loader-wrapper-header.js
@@ -1,3 +1,1 @@
 ;(function () { return typeof window.QAS === 'function' ? window.QAS : Array.prototype.push.bind(window._qas_queue ? window._qas_queue : (window._qas_queue = []))}()(function () {
-    
-}));
